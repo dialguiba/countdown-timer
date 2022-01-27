56974c8320c93062ed84c569ae15b30066fdc083
dialguiba <dialguiba@gmail.com>
2022-01-27 17:50:10 -0500

    add hash router for gh pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { CountdownTimerApp } from './CountdownTimerApp';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import { CountdownTimerApp } from "./CountdownTimerApp";
 
-import "./styles/base/_base.scss"
-import "./styles/base/_settings.scss"
+import "./styles/base/_base.scss";
+import "./styles/base/_settings.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <CountdownTimerApp />
+    <HashRouter>
+      <CountdownTimerApp />
+    </HashRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
